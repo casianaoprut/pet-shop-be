e1b64a9d9aaa6ac2c74402f45a577c5443695da1
casianaoprut <oprunicoletacasiana@yahoo.com>
2021-11-01 13:36:33 +0200

    initial commit

---
+++ src/test/java/webapp/pickme/petshop/PetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp.pickme.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
