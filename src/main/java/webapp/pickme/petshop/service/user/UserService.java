8223830308bf961c197c1aab034679eeeb420b62
casianaoprut <oprunicoletacasiana@yahoo.com>
2021-11-09 01:34:11 +0200

    Improved creation process of an admin accounts.

--- src/main/java/webapp/pickme/petshop/service/user/UserService.java
+++ src/main/java/webapp/pickme/petshop/service/user/UserService.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static webapp.pickme.petshop.data.model.user.Role.*;
+
 @Service
 public class UserService {
 
@@ -32,29 +34,38 @@ public class UserService {
     public UserView create(MyUser myUser) throws UserException {
         if(!checkIfUsernameAlreadyExists(myUser.getUserName())) {
             var encodedPassword = passwordEncoder.encode(myUser.getPassword());
+            if( createAdminAccountCondition(myUser)){
+                throw new UserException("Unauthorized to make an admin account!");
+            }
             var user = new User(myUser.getUserName(), encodedPassword, getAuthorities(myUser.getRole()));
             jdbcUserDetailsManager.createUser(user);
-            return new UserView(user.getUsername(), getRole(user));
+            return new UserView(user.getUsername(), getRoleFromAuthorities(user));
         }
         throw new UserException("Username already exists!");
     }
 
+    public UserView login(){
+        var username = getAuthenticatedUserName();
+        var role = getRoleFromAuthorities(jdbcUserDetailsManager.loadUserByUsername(username));
+        return new UserView(username, role);
+    }
+
+    private boolean createAdminAccountCondition(MyUser myUser) throws UserException {
+        return myUser.getRole() != null &&
+               myUser.getRole().equals(ADMIN) &&
+               !getRoleFromAuthorities(getAuthenticatedUser()).equals(ADMIN);
+    }
+
     private List<GrantedAuthority> getAuthorities(Role role){
         List<GrantedAuthority> authorities = new ArrayList<>(List.of());
-        authorities.add(new SimpleGrantedAuthority(Optional.ofNullable(role).orElse(Role.USER).name()));
+        authorities.add(new SimpleGrantedAuthority(Optional.ofNullable(role).orElse(USER).name()));
         return authorities;
     }
 
-    private Role getRole(UserDetails user){
+    private Role getRoleFromAuthorities(UserDetails user){
         var authorities = user.getAuthorities();
         return authorities.stream()
-                    .map(elem -> Role.valueOf(elem.toString())).findFirst().orElse(null);
-    }
-
-    public UserView login(){
-        var username = getAuthenticatedUserName();
-        var role = getRole(jdbcUserDetailsManager.loadUserByUsername(username));
-        return new UserView(username, role);
+                    .map(elem -> valueOf(elem.toString())).findFirst().orElse(null);
     }
 
     private boolean checkIfUsernameAlreadyExists(String username){
@@ -68,10 +79,17 @@ public class UserService {
 
     public String getAuthenticatedUserName(){
         var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        if(principal instanceof UserDetails){
+        if(principal instanceof UserDetails)
             return ((UserDetails) principal).getUsername();
-        }
         return principal.toString();
     }
+
+    public UserDetails getAuthenticatedUser() throws UserException {
+        var username = getAuthenticatedUserName();
+        if(username.equals("anonymousUser")){
+            throw new UserException("Admin not authenticated!");
+        }
+        return jdbcUserDetailsManager.loadUserByUsername(username);
+    }
+
 }
